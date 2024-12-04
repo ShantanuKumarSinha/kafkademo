537032ba520a0007dc1ee639005fac7efff97171
Shantanu Kumar <shan.raj93@gmail.com>
2024-12-04 22:47:00 +0530

    (feat/logger): add log in ConsumerService

--- src/main/java/com/example/kafka/demo/service/ConsumerService.java
+++ src/main/java/com/example/kafka/demo/service/ConsumerService.java
@@ -1,15 +1,23 @@
 package com.example.kafka.demo.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
+
 public class ConsumerService {
+
+    Logger log = LoggerFactory.getLogger(ConsumerService.class);
 
 
     @KafkaListener(topics = "KafkaDemoTopics", groupId ="KafkaDemoGroup" )
     public void listenKafkaMessage(String messageRecieved){
         System.out.println("The message received is "+messageRecieved);
+        log.info(messageRecieved);
+
+
     }
 
 }
